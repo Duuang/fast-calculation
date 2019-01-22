3dce7b296cceec1136b5ba2fbd766276da8d116e
cky <15201692553@126.com>
2019-01-22 10:23:32 +0800

    更新Fraction类
    添加ToString()函数，将分数转换为string字符串
    完善注释

--- Fraction.cpp
+++ Fraction.cpp
@@ -1,9 +1,16 @@
 #include "Fraction.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 Fraction::Fraction() {
 
 }
 
+//
+//设置分子分母，自动设置isinteger_
+//
 Fraction::Fraction(int numerator, int denominator) {
   numerator_ = numerator;
   denominator_ = denominator;
@@ -15,6 +22,9 @@ Fraction::Fraction(int numerator, int denominator) {
   this->Simplify();
 }
 
+//
+//设置分子分母，自动设置isinteger_
+//
 void Fraction::Set(int numerator, int denominator) {
   numerator_ = numerator;
   denominator_ = denominator;
@@ -26,16 +36,23 @@ void Fraction::Set(int numerator, int denominator) {
   this->Simplify();
 }
 
+//
+//分数化简，直接更改原对象，并返回*this
+//
 Fraction Fraction::Simplify() {
+  //如果分母是1，或者分子是0
   if (denominator_ == 1 || numerator_ == 0) {
+    //分子是0，将分母改为1，整数标识改1
     if (numerator_ == 0) {
       denominator_ = 1;
       isinteger_ = true;
       return *this;
     }
+    //不是0，分母是1，则整数标志改1
     isinteger_ = true;
     return *this;
   }
+  //遍历小于分母的数，看能不能同时整除分子分母，直到没有这样的数，或者分母为1
   while (true) {
     int i;
     int currentdenominator = denominator_;
@@ -46,40 +63,62 @@ Fraction Fraction::Simplify() {
         break;
       }
     }
+    //分母为1，则整数标志设1，直接返回
     if (denominator_ == 1) {
       isinteger_ = true;
       return *this;
     }
-    if (i == currentdenominator + 1)
+    //整数标志设0，返回
+    if (i == currentdenominator + 1) {
+      isinteger_ = false;
       return *this;
+    }
   }
 }
 
+//
+//相加
+//
 Fraction Fraction::Add(Fraction fraction) {
   int numerator = fraction.GetDenominator() * numerator_ + fraction.GetNumerator() * denominator_;
   int denominator = fraction.GetDenominator() * denominator_;
   Fraction result(numerator, denominator);
   return result.Simplify();
 }
+//
+//相减
+//
 Fraction Fraction::Subtract(Fraction fraction) {
   int numerator = fraction.GetDenominator() * numerator_ - fraction.GetNumerator() * denominator_;
   int denominator = fraction.GetDenominator() * denominator_;
   Fraction result(numerator, denominator);
   return result.Simplify();
 }
+//
+//相乘
+//
 Fraction Fraction::Multiply(Fraction fraction) {
   int numerator = fraction.GetNumerator() * numerator_;
   int denominator = fraction.GetDenominator() * denominator_;
   Fraction result(numerator, denominator);
   return result.Simplify();
 }
+//
+//除以
+//
 Fraction Fraction::DividedBy(Fraction fraction) {
   return this->Multiply(fraction.Reciprocal());
 }
+//
+//倒数
+//
 Fraction Fraction::Reciprocal() {
   Fraction result(denominator_, numerator_);
   return result;
 }
+//
+//乘方，返回原对象的exp次方
+//
 Fraction Fraction::Power(int exp) {
   Fraction result(1, 1);
   for (int i = 0; i < exp; i++) {
@@ -88,12 +127,42 @@ Fraction Fraction::Power(int exp) {
   return result.Simplify();
 }
 
+//
+//返回字符串形式
+//
+string Fraction::ToString() {
+  char buffer[105];
+  //如果是整数
+  if (isinteger_) {
+    _itoa_s(numerator_, buffer, 10);
+    string result = buffer;
+    return result;
+  //如果是分数
+  } else {
+    _itoa_s(numerator_, buffer, 10);
+    string result = buffer;
+    result.push_back('/');
+    _itoa_s(denominator_, buffer, 10);
+    result.append(buffer);
+    return result;
+  }
+}
+
+//
+//获取成员变量numerator_
+//
 int Fraction::GetNumerator() {
   return numerator_;
 }
+//
+//获取成员变量denominator_
+//
 int Fraction::GetDenominator() {
   return denominator_;
 }
+//
+//获取成员变量isinteger_
+//
 bool Fraction::IsInteger() {
   return isinteger_;
 }
--- Fraction.h
+++ Fraction.h
@@ -1,5 +1,7 @@
 #pragma once
-
+#include <string>
+#include <iostream>
+using namespace std;
 //
 //保存分数值，并进行运算
 //
@@ -12,19 +14,27 @@ public:
   void Set(int numerator, int denominator);
   //化简，返回Fraction类，更改原对象
   Fraction Simplify();
-  //加减乘除另一个分数，不更改原对象，返回一个新对象
+  //加另一个分数，不更改原对象，返回一个新对象
   Fraction Add(Fraction fraction);
+  //减另一个分数，不更改原对象，返回一个新对象
   Fraction Subtract(Fraction fraction);
+  //乘另一个分数，不更改原对象，返回一个新对象
   Fraction Multiply(Fraction fraction);
+  //除另一个分数，不更改原对象，返回一个新对象
   Fraction DividedBy(Fraction fraction);
   //倒数，不更改原对象，返回一个新对象
   Fraction Reciprocal();
-  //乘方，exp为指数，，不更改原对象，返回一个新对象
+  //乘方，exp为指数，不更改原对象，返回一个新对象
   Fraction Power(int exp);
+  //转化为string字符串，整数为十进制整数，分数为a/b，不带空格
+  string ToString();
   //得到成员变量值
   int GetNumerator();
+  //得到成员变量值
   int GetDenominator();
+  //得到成员变量值
   bool IsInteger();
+
 private:
   //分子
   int numerator_; 
