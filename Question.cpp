5b991650206156b7c611ac51ffa6df850842ed6c
cky <15201692553@126.com>
2019-01-22 11:43:18 +0800

    Question类完成（不包含嵌套类）
    完善注释

--- Question.cpp
+++ Question.cpp
@@ -7,18 +7,38 @@ Question::Question() {
   if_pow_operator = false;
 }
 
+//
+//设置问题种类，整数/分数/整数分数混合，和有无乘方运算符
+//
+int Question::SetType(enum NumberType number_type, bool if_pow_operator) {
+  this->number_type = number_type;
+  this->if_pow_operator = if_pow_operator;
+}
+
+//
+//生成amount数量的题目，并保存在字符串question_str
+//
 int Question::GenerateAndSave(int amount) {
   Question::QuestionGenerator generator;
   //调用QuestionGenerator类的Generate()
   for (int i = 0; i < amount; i++) {
-    generator.Generate(*this);
+    generator.GenerateOne(*this);
   }
-  
   return 1;
 }
 
+//
+//计算index处的题目结果（index取值0 ~ amount-1)
+//
 Fraction Question::Calculate(int index) {
   Question::QuestionCalculator calculator;
   return calculator.Calculate(index, *this);
   
+}
+
+//
+//得到index处题目的string字符串（index取值0 ~ amount-1)
+//
+string Question::GetQuestion(int index) {
+  return question_str[index];
 }
