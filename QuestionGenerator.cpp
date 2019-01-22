5d3f8016fd75b6b44b98e50f990a36fe587045dd
ChangyunHan <45808573+ChangyunHan@users.noreply.github.com>
2019-01-23 01:55:35 +0800

    生成（添加乘方运算

--- QuestionGenerator.cpp
+++ QuestionGenerator.cpp
@@ -6,72 +6,90 @@
 #include <stdlib.h>
 
 using namespace std;
+int Question::QuestionGenerator::Random(int x, int y) {
+	std::random_device rd;
+	std::minstd_rand rng{ rd() };
+	std::uniform_int_distribution<long> dist{ x, y };
+	return dist(rng);
+}
 
-char Question::QuestionGenerator::operate_char()
+string  Question::QuestionGenerator::operate_char(enum IfPowOperator n)
 {
-	 char symbol[1];
-	 int j,num=4;
-	 
-	 j=rand()%num;
-	 if(j==0) symbol[0]='+';
-    else if(j==1) symbol[0]='-';
-    else if(j==2) symbol[0]='*';
-    else symbol[0]='/';
-	    return symbol[0];
- } 
+	string symbol;
+	int j, num;
+	if (n == NONE)
+		num = 4;
+	else
+		num = 9;
+
+	j = Random(0,num-1);
+	if (j/2 == 0)
+		symbol = '+';
+	else if (j/2 == 1)
+		symbol = '-';
+	else if (j/2 == 2)
+		symbol = '*';
+	else if (j/2 == 3)
+		symbol = '/';
+	else if (n == OPERATOR_1)
+		symbol = '^';
+	else
+		symbol = "**";
+	return symbol;
+}
  int Question::QuestionGenerator::GenerateOne(Question &question)
  {
 	 if(question.number_type == ALL_INTEGER)		//纯整数
 
 	 {
-		 string question;
+		 string questions;
 		 string operate_num1;
 		 string operate_num2;
-		 char symbol;
+		 string symbol;
 
-		 int operate_number = rand() % 3 + 2;
-		 int num1 = rand() % 99 + 1;
-		 int num2 = rand() % 99 + 1;
-		 symbol = operate_char();
+		 int operate_number = Random(2,5);
+		 int num1 = Random(1,20);
+		 int num2 = Random(1,20);
+		 symbol = operate_char(question.if_pow_operator);
 
 		 char zhuanhuan[100];
-		 itoa(num1, zhuanhuan, 10);
+		 _itoa_s(num1, zhuanhuan, 10);
 		 operate_num1 = zhuanhuan;
-		 itoa(num2, zhuanhuan, 10);
+		 _itoa_s(num2, zhuanhuan, 10);
 		 operate_num2 = zhuanhuan;
-		 question = operate_num1 + ' ' + symbol + ' ' + operate_num2;
+		 questions = operate_num1 + ' ' + symbol + ' ' + operate_num2;
 
 		 if (operate_number > 2)
 		 {
 			 for (; operate_number > 2; operate_number--)
 			 {
-				 symbol = operate_char();
-				 num2 = rand() % 99 + 1;
-				 itoa(num2, zhuanhuan, 10);
+				 symbol = operate_char(question.if_pow_operator);
+				 num2 = Random(1,30);
+				 _itoa_s(num2, zhuanhuan, 10);
 				 operate_num2 = zhuanhuan;
-				 operate_num1 = question;
-				 int bracket = rand() % 3;
+				 operate_num1 = questions;
+				 int bracket = Random(0,2);
 				 if (bracket == 2)
 					 operate_num1 = '(' + operate_num1 + ')';
-				 question = operate_num1 + ' ' + symbol + ' ' + operate_num2;
+				 questions = operate_num1 + ' ' + symbol + ' ' + operate_num2;
 
 			 }
 		 }
-		 (question.question_str + question.amount)->append(question);
+		 (question.question_str + question.amount)->append(questions);
 		 question.amount++;
 		 return 1;
 
 	 }
 	 else if (question.number_type == ALL_FRACTION)			//纯小数
 	 {
-		 string question;
+		 string questions;
 		 int numerator;
 		 int denominator;
-		 int operate_number = rand() % 9 + 2;
+		 int operate_number = Random(2,5);
 
 		 Fraction fraction1;
-		 numerator = rand() % 99 + 1;
-		 denominator = rand() % 99 + 1;
+		 numerator = Random(1, 10);
+		 denominator = Random(1,10);
 		 if (numerator > denominator)
 		 {
 			 int buffer = numerator;
@@ -81,8 +99,8 @@ char Question::QuestionGenerator::operate_char()
 		 fraction1.Set(numerator, denominator);
 
 		 Fraction fraction2;
-		 numerator = rand() % 99 + 1;
-		 denominator = rand() % 99 + 1;
+		 numerator = Random(1, 10);
+		 denominator = Random(1,10);
 		 if (numerator > denominator)
 		 {
 			 int buffer = numerator;
@@ -91,17 +109,17 @@ char Question::QuestionGenerator::operate_char()
 		 }
 		 fraction2.Set(numerator, denominator);
 
-		 char symbol = operate_char();
+		 string symbol = operate_char(question.if_pow_operator);
 		 string operate_fraction1 = fraction1.ToString();
 		 string operate_fraction2 = fraction2.ToString();
-		 question = operate_fraction1 + ' ' + symbol + ' ' + operate_fraction2;
+		 questions = operate_fraction1 + ' ' + symbol + ' ' + operate_fraction2;
 		 if (operate_number > 2)
 		 {
 			 for (; operate_number > 2; operate_number--)
 			 {
-				 symbol = operate_char();
-				 numerator = rand() % 99 + 1;
-				 denominator = rand() % 99 + 1;
+				 symbol = operate_char(question.if_pow_operator);
+				 numerator = Random(1, 10);
+				 denominator = Random(1,10);
 				 if (numerator > denominator)
 				 {
 					 int buffer = numerator;
@@ -110,42 +128,42 @@ char Question::QuestionGenerator::operate_char()
 				 }
 				 fraction2.Set(numerator, denominator);
 				 operate_fraction2 = fraction2.ToString();
-				 operate_fraction1 = question;
-				 int bracket = rand() % 3;
+				 operate_fraction1 = questions;
+				 int bracket = Random(0, 2);
 				 if (bracket == 2)
 					 operate_fraction1 = '(' + operate_fraction1 + ')';
-				 question = operate_fraction1 + ' ' + symbol + ' ' + operate_fraction2;
+				 questions = operate_fraction1 + ' ' + symbol + ' ' + operate_fraction2;
 
 			 }
 		 }
-		 (question.question_str + question.amount)->append(question);
+		 (question.question_str + question.amount)->append(questions);
 		 question.amount++;
 		 return 1;
 	 }
 	 else									//混合
 	{
-		string question;
+		string questions;
 		string operate1;
 		string operate2;
-		char symbol;
-		int operate_number = rand() % 3 + 2;
+		string symbol;
+		int operate_number = Random(2, 5);
 
 		int flag;
-		flag = rand() % 2;
+		flag = Random(0,1);
 		if (flag == 0)
 		{
 			int num;
-			num = rand() % 99 + 1;
+			num = Random(1, 20);
 			char zhuanhuan[100];
-			itoa(num1, zhuanhuan, 10);
+			_itoa_s(num, zhuanhuan, 10);
 			operate1 = zhuanhuan;
 
 		 }
 		else
 		{
 			Fraction fraction;
-			int numerator = rand() % 99 + 1;
-			int denominator = rand() % 99 + 1;
+			int numerator = Random(1, 10);
+			int denominator = Random(1, 10);
 			if (numerator > denominator)
 			{
 				int buffer = numerator;
@@ -157,21 +175,21 @@ char Question::QuestionGenerator::operate_char()
 		}
 
 
-		flag = rand() % 2;
+		flag = Random(0, 1);
 		if (flag == 0)
 		{
 			int num;
-			num = rand() % 99 + 1;
+			num = Random(1, 20);
 			char zhuanhuan[100];
-			itoa(num1, zhuanhuan, 10);
+			_itoa_s(num, zhuanhuan, 10);
 			operate2 = zhuanhuan;
 
 		}
 		else
 		{
 			Fraction fraction;
-			int numerator = rand() % 99 + 1;
-			int denominator = rand() % 99 + 1;
+			int numerator = Random(1, 10);
+			int denominator = Random(1, 10);
 			if (numerator > denominator)
 			{
 				int buffer = numerator;
@@ -182,29 +200,29 @@ char Question::QuestionGenerator::operate_char()
 			operate2 = fraction.ToString();
 		}
 
-		symbol = operate_char();
-		question = operate1 + ' ' + symbol + ' ' + operate2;
+		symbol = operate_char(question.if_pow_operator);
+		questions = operate1 + ' ' + symbol + ' ' + operate2;
 
 		if (operate_number > 2)
 		{
 			for (; operate_number > 2; operate_number--)
 			{
-				symbol = operate_char();
-				flag = rand() % 2;
+				symbol = operate_char(question.if_pow_operator);
+				flag = Random(0,1);
 				if (flag == 0)
 				{
 					int num;
-					num = rand() % 99 + 1;
+					num = Random(1, 20);
 					char zhuanhuan[100];
-					itoa(num1, zhuanhuan, 10);
+					_itoa_s(num, zhuanhuan, 10);
 					operate2 = zhuanhuan;
 
 				}
 				else
 				{
 					Fraction fraction;
-					int numerator = rand() % 99 + 1;
-					int denominator = rand() % 99 + 1;
+					int numerator = Random(1, 10);
+					int denominator = Random(1, 10);
 					if (numerator > denominator)
 					{
 						int buffer = numerator;
@@ -214,15 +232,15 @@ char Question::QuestionGenerator::operate_char()
 					fraction.Set(numerator, denominator);
 					operate2 = fraction.ToString();
 				}
-				operate1 = question;
-				int bracket = rand() % 3;
+				operate1 = questions;
+				int bracket = Random(0, 2);
 				if (bracket == 2)
-					operate1 = '(' + operate_num1 + ')';
-				question = operate1 + ' ' + symbol + ' ' + operate2;
+					operate1 = '(' + operate1 + ')';
+				questions = operate1 + ' ' + symbol + ' ' + operate2;
 
 			}
 		}
-		(question.question_str + question.amount)->append(question);
+		(question.question_str + question.amount)->append(questions);
 		question.amount++;
 		return 1;
 
