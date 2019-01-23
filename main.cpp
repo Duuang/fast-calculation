58cc6a3e6996de1412906c912362cac92863b92a
cky <15201692553@126.com>
2019-01-23 22:24:13 +0800

    完成main.cpp，命令行程序完成
    分为两种模式，
    无命令行参数输入时，分步提示用户输入参数
    或者一口气输入所有命令行参数，输入方式程序有提示

--- main.cpp
+++ main.cpp
@@ -1,35 +1,79 @@
 #include "Question.h"
 #include <iostream>
 #include <string>
+#include <stdio.h>
 using namespace std;
 
 
-int main() {
+int main(int argc, char *argv[]) {
   Question question;
-  question.SetType(Question::NumberType::ALL_INTEGER, Question::IfPowOperator::OPERATOR_2);
-  /*question.question_str[0];
-  question.question_str[1];
-  question.amount = 0;
-  Question::QuestionGenerator tmp;
-  int a = tmp.IfSame(question, "2 + 1 + 1");
-  question.question_str[0] = "2 + 1 + 1";
-  question.amount++;
-   a = tmp.IfSame(question, "2 + (1 + 1)");*/
-   //question.question_str[0] = "((20 + 12) / 15) * 23";
-//   ((20 + 12) / 15) * 23
-//15
-//15 - 4 / 12
-//44/3
-//7 - 5 / 11
-//72/11
-//   
-
-   question.GenerateAndSave(100);
-
-  for (int i = 0; i < 100; i++) {
-  //cout << question. << endl;
-    cout << question.GetQuestion(i) << endl;
-    cout << question.Calculate(i).ToString() << endl;
+  //无命令行参数的情况，分步提示用户输入参数
+  if (argc == 1) {
+    int count = 0;
+    cout << "请输入生成的题目数字种类，0为纯整数，1为纯分数，2为整数分数混合\n";
+    int number_type = -1;
+    cin >> number_type;
+    while (number_type < 0 || number_type > 2) {
+      cout << "错误数值\n";
+      cin >> number_type;
+    }
+    cout << "请输入生成的题目的乘方种类，0为不带乘方，1为用^表示乘方，2为用**表示乘方\n";
+    int if_pow = -1;
+    cin >> if_pow;
+    while (if_pow < 0 || if_pow > 2) {
+      cout << "错误数值\n";
+      cin >> if_pow;
+    }
+    cout << "请输入生成的题目的数量，1~1000\n";
+    int amount = -1;
+    cin >> amount;
+    while (amount < 1 || amount > 1000) {
+      cout << "错误数值\n";
+      cin >> amount;
+    }
+    question.SetType((enum Question::NumberType) (number_type + 1), (enum Question::IfPowOperator) if_pow);
+    question.GenerateAndSave(amount);
+    for (int i = 0; i < amount; i++) {
+      cout << "题目" << i + 1 << "为： " << question.GetQuestion(i) << "    请输入答案：" << endl;
+      string answer;
+      cin >> answer;
+      if (answer == question.Calculate(i).ToString()) {
+        cout << "正确\n";
+        count++;
+      } else {
+        cout << "错误，答案应为" << question.Calculate(i).ToString() << endl;
+      }
+    }
+    cout << "正确题数：" << count << "\n错误题数：" << amount - count << endl;
+  //有命令行参数的情况，如果输入参数错误，提示样例输入
+  } else if (argc == 6) {
+    int count = 0;
+    if (strcmp(argv[1], "-generate") == 0 && strcmp(argv[3], "-settings") == 0
+      && atoi(argv[2]) >= 1 && atoi(argv[2]) <= 1000 && atoi(argv[4]) >= 0 && atoi(argv[4]) <= 2
+      && atoi(argv[5]) >= 0 && atoi(argv[5]) <= 2) {
+      question.SetType((enum Question::NumberType) (atoi(argv[4]) + 1), (enum Question::IfPowOperator) atoi(argv[5]));
+      question.GenerateAndSave(atoi(argv[2]));
+      for (int i = 0; i < atoi(argv[2]); i++) {
+        cout << "题目" << i + 1 << "为： " << question.GetQuestion(i) << "    请输入答案：" <<endl;
+        string answer;
+        cin >> answer;
+        if (answer == question.Calculate(i).ToString()) {
+          cout << "正确\n";
+          count++;
+        } else {
+          cout << "错误，答案应为" << question.Calculate(i).ToString() << endl;
+        }
+      }
+      cout << "正确题数：" << count << "\n错误题数：" << atoi(argv[2]) - count << endl;
+    } else {
+      printf("请输入正确的参数！\n命令行参数为：无参数，或者-generate 1~1000 -settings 0/1/2 0/1/2\n\
+如：-generate 100 -settings 0 0\n其中第一个1~1000的数为生成题目数量，第2个0/1/2的参数代表题目为纯整数/纯分数/整数分数混合，\
+第3个0/1/2的参数代表题目为无乘方/用^表示的乘方/用**表示的乘方\n");
+    }
+  } else {
+    printf("请输入正确的参数！\n命令行参数为：无参数，或者-generate 1~1000 -settings 0/1/2 0/1/2\n\
+如：-generate 100 -settings 0 0\n其中第一个1~1000的数为生成题目数量，第2个0/1/2的参数代表题目为纯整数/纯分数/整数分数混合，\
+第3个0/1/2的参数代表题目为无乘方/用^表示的乘方/用**表示的乘方\n");
   }
   system("pause");
 }
